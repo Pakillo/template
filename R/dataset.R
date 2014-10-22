73539994a6b8ddee7a4f9e510a4806725b6dbde2
Pakillo <f.rodriguez.sanc@gmail.com>
2014-10-22 19:15:27 +0200

    minor update of dataset documentation

--- R/dataset.R
+++ R/dataset.R
@@ -3,6 +3,7 @@
 #' This is an example showing how to document datasets included in the package (in \code{data} folder):
 #' you must specify docType and name; do not forget NULL in the end.
 #' Can include other fields, like citation.
+#' See \url{http://r-pkgs.had.co.nz/data.html} for further explanations.
 #'
 #' @docType data
 #' @name exampledataset
