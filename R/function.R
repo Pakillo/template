df1faec3a5a1c8c0d76fb5e4b6ebb728b0b8a5b3
Pakillo <f.rodriguez.sanc@gmail.com>
2014-10-23 19:33:27 +0200

    added example documentation for functions

---
+++ R/function.R
@@ -0,0 +1,15 @@
+#' How to document functions.
+#'
+#' This is an example showing how to document functions to be included in the package.
+#'
+#' @export
+#' @name examplefunction
+#' @author Francisco Rodriguez-Sanchez
+#' @param text Message to print.
+#' @seealso \url{http://r-pkgs.had.co.nz/man.html}
+#' @examples
+#' examplefunction('Hello world!')
+
+examplefunction <- function(text){
+  cat(text)
+}
