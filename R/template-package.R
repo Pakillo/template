dd1c1f546b07340779cb2e4fabb82e49bbaeecf6
Pakillo <f.rodriguez.sanc@gmail.com>
2014-10-24 13:48:22 +0200

    updated README with steps to use the package

--- R/template-package.R
+++ R/template-package.R
@@ -15,17 +15,17 @@
 #'
 #' 1. Clone or download zip file of this repository. Unzip.
 #' 2. Choose a name for your project.
-#' 3. Change the name of the package in DESCRIPTION file and template-package.R to your chosen name.
-#' 4. Place original (raw) data (e.g. in csv or txt format) in inst/extdata.
-#' 5. Place any function you write in R/ folder.
-#' 6. Write tests for your functions (see http://r-pkgs.had.co.nz/tests.html) and place them in `tests` folder.
-#' 7. Save all R scripts used for data preparation in the `data-raw` folder.
-#' 8. Save final clean, tidy datasets (in `.rda` format, using `save`) in the `data` folder.
-#' 9. Choose an Rmarkdown template (e.g. from the `rmdTemplates` or `rticles` packages) or create your own Rmd document and place it in the `vignettes` folder. Remember that this Rmd document must include info about knitr as the vignette engine, i.e.:
+#' 3. Change the name of the package in `DESCRIPTION` file and `template-package.R` to your chosen name.
+#' 4. Place original (raw) data (e.g. in csv or txt format) in `inst/extdata`.
+#' 5. If you write custom functions for the analyses, place them in `R` folder. Write tests for your functions (see http://r-pkgs.had.co.nz/tests.html) and place them in `tests` folder.
+#' 6. Save all R scripts used for data preparation in the `data-raw` folder.
+#' 7. Save final (clean, tidy) datasets in `.rda` format (using `save`) in the `data` folder.
+#' 8. Choose an Rmarkdown template (e.g. from [rmdTemplates](https://github.com/Pakillo/rmdTemplates) or [rticles](https://github.com/rstudio/rticles) packages) or create your own Rmd document and place it in the `vignettes` folder. Remember that this Rmd document must include info about knitr as the vignette engine, i.e. this bit:
 #'   +<!--
-#' +%\VignetteEngine{knitr}
+#'   +%\VignetteEngine{knitr}
 #' +%\VignetteIndexEntry{Vignette title}
 #' +-->
-#' 10. Press the Knit button or use devtools::build_vignettes()
+#'
+#' 9. Press the Knit button or use devtools::build_vignettes() to render the report.
 #'
 NULL
